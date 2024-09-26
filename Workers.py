9230ff6364e2ca69e6d9c08bd4b8846d05737747
Eugene <30446748+EugeneZakhno@users.noreply.github.com>
2024-09-26 16:21:18 +0300

    can't install package diagrams

--- Workers.py
+++ Workers.py
@@ -4,8 +4,8 @@ from diagrams.aws.database import RDS
 from diagrams.aws.network import ELB
 
 with Diagram("Grouped Workers", show=False, direction="TB"):
-    ELB("lb") >> [EC2("worker1"),
+    ELB("lb") >> [EC2("worker0"),
+                  EC2("worker1"),
                   EC2("worker2"),
                   EC2("worker3"),
-                  EC2("worker4"),
-                  EC2("worker5")] >> RDS("events")
+                  EC2("worker4")] >> RDS("events")
